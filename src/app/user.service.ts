ca96b3caf9233fcb1e3a7f3e9e3ec52e9e4ddf0b
Karan Jadhav <karanmjadhav@gmail.com>
2021-01-24 10:57:49 +0530

    IP changed

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -15,29 +15,29 @@ export class UserService {
 
   createUser(user: User):Observable<User>|any  {
 
-    return this.httpClient.post("http://ec2-3-90-222-9.compute-1.amazonaws.com.com:8080/adduser",user);
+    return this.httpClient.post("http://ec2-54-234-103-17.compute-1.amazonaws.com.com:8080/adduser",user);
 
   }
 
   loginUser(user:User):Observable<User[]>{
-    return this.httpClient.post<User[]>("http://ec2-3-90-222-9.compute-1.amazonaws.com:8080/login",user);
+    return this.httpClient.post<User[]>("http://ec2-54-234-103-17.compute-1.amazonaws.com:8080/login",user);
   }
 
   forgetPassword(user:User):Observable<User>|any {
-    return this.httpClient.post("http://ec2-3-90-222-9.compute-1.amazonaws.com:8080/forgetpassword",user);
+    return this.httpClient.post("http://ec2-54-234-103-17.compute-1.amazonaws.com:8080/forgetpassword",user);
   }
 
-  private changePassURL = "http://ec2-3-90-222-9.compute-1.amazonaws.com:8080/changepassword";
+  private changePassURL = "http://ec2-54-234-103-17.compute-1.amazonaws.com:8080/changepassword";
   changePassword(userName:String|any, user:User): Observable<Object>{
     return this.httpClient.put(`${this.changePassURL}/${userName}`, user);
   }
 
-  private getUserByNameURL = "http://ec2-3-90-222-9.compute-1.amazonaws.com:8080/getUser";
+  private getUserByNameURL = "http://ec2-54-234-103-17.compute-1.amazonaws.com:8080/getUser";
   getUserByName(userName:String|undefined): Observable<User>{
     return this.httpClient.get<User>(`${this.getUserByNameURL}/${userName}`);
   }
 
-  private getUserByEmailURL = "http://ec2-3-90-222-9.compute-1.amazonaws.com:8080/getUserByEmail";
+  private getUserByEmailURL = "http://ec2-54-234-103-17.compute-1.amazonaws.com:8080/getUserByEmail";
   getUserByEmailId(emailId:String|any): Observable<User>{
     return this.httpClient.get<User>(`${this.getUserByEmailURL}/${emailId}`);
   }
